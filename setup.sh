c31d803ea5dc56f8139e3f7b8e273e58e0101a74
egbendito <e.gbendito@gmail.com>
2024-01-11 14:06:47 +0000

    fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-Rscript functions/01_download/00_0_skeleton.R > setup.log # Need to check because the ROI is not properly generated... MOZ is missing
-Rscript functions/01_download/01_0_download_gadm.R >> setup.log 2>&1
-Rscript functions/01_download/01_5_download_chirts.R >> setup.log 2>&1
-Rscript functions/01_download/01_2_download_chirps.R >> setup.log 2>&1
-Rscript functions/01_download/01_1_download_agera5.R >> setup.log 2>&1
-Rscript functions/01_download/01_3_download_isda.R >> setup.log 2>&1
+Rscript R/01_download/00_0_skeleton.R > setup.log # Need to check because the ROI is not properly generated... MOZ is missing
+Rscript R/01_download/01_0_download_gadm.R >> setup.log 2>&1
+Rscript R/01_download/01_5_download_chirts.R >> setup.log 2>&1
+Rscript R/01_download/01_2_download_chirps.R >> setup.log 2>&1
+Rscript R/01_download/01_1_download_agera5.R >> setup.log 2>&1
+Rscript R/01_download/01_3_download_isda.R >> setup.log 2>&1
 
 # Need to create the skeleton with the dssat (culfiles, xfiles and _ERR.WTH and _ERR.SOL) and the user folders
 
