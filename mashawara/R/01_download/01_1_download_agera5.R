d51a91b7c7a199b5a96d84c16f28d8a878bd79fd
egbendito <e.gbendito@gmail.com>
2024-02-27 12:51:53 +0000

    update 01_1_download_agera5.R
    fix sources

--- mashawara/R/01_download/01_1_download_agera5.R
+++ mashawara/R/01_download/01_1_download_agera5.R
@@ -1,7 +1,6 @@
-vars <- c("2m_relative_humidity", "solar_radiation_flux", "2m_temperature-24_hour_mean", "2m_temperature-24_hour_maximum", "2m_temperature-24_hour_minimum", "10m_wind_speed-24_hour_mean")
+vars <- c("RelativeHumidity", "SolarRadiation", "TemperatureMean", "TemperatureMax", "TemperatureMin", "WindSpeed")
 names(vars) <- c("RelativeHumidity", "SolarRadiation", "TemperatureMean", "TemperatureMax", "TemperatureMin", "WindSpeed")
 years <- seq(1990, as.integer(format(Sys.Date(), "%Y"))-1)
-months <- as.character(sprintf("%02d", 1:12))
 pol <- terra::vect("./data/inputs/main/administrative/roi.gpkg", layer = "roi")
 bb <- terra::ext(pol)
 origin<- getwd()
@@ -13,24 +12,13 @@ for (i in seq_along(vars)) {
   setwd(origin)
   vname <- names(vars[i])
   v <- vars[i][[1]]
+  dir.create(path = paste0("./data/inputs/main/weather/historical/", vname), recursive = TRUE, showWarnings = FALSE)
   for (year in years){
     if(!file.exists(paste0("./data/inputs/main/weather/historical/", vname, "/", year, ".nc"))){
-      dir.create(path = paste0("./data/inputs/main/weather/historical/", vname, "/tmp"), recursive = TRUE, showWarnings = FALSE)
-      for (month in months){
-        suff <- ifelse(v %in% c("2m_relative_humidity", "solar_radiation_flux"), "-NA", "")
-        unzip(paste0("/home/jovyan/common_data/ecmwf_agera5/", v, suff, "-", year, "-", month, ".zip"),
-              exdir = paste0("./data/inputs/main/weather/historical/", vname, "/tmp/", v))
-      }
-      R <- terra::rast()
-      for (file in list.files(paste0("./data/inputs/main/weather/historical/", vname, "/tmp/", v), full.names = TRUE)) {
-        r <- terra::rast(file)
-        terra::add(R) <- r
-      }
+      R <- terra::rast(paste0("/home/jovyan/common_data/ecmwf_agera5/netcdf/", vname, "/AgEra/", year, ".nc"))
       R <- terra::crop(R, bb)
       terra::writeCDF(R, filename = paste0("./data/inputs/main/weather/historical/", vname, "/", year, ".nc"), prec = "float", compression = 5, overwrite = TRUE)
-      do.call(file.remove, list(list.files(paste0("./data/inputs/main/weather/historical/", vname, "/tmp/", v), full.names = TRUE)))
     }
-    unlink(paste0("./data/inputs/main/weather/historical/", vname, "/tmp"), recursive = TRUE)
   }
 }
 
